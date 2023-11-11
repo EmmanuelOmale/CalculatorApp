9cbdf82422cffbafb251fa8331213f47b45f9134
EmmanuelOmale <eomale@gmail.com>
2023-11-11 13:10:10 +0100

    Calculator service and interface registered in IOC

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,8 +1,12 @@
+using CalculatorApp.Services;
+using CalculatorApp.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<ICalculatorService, CalculatorService>();    
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
