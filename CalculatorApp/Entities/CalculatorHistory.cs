97cace0245130f4e07d0b11df332d25009039e29
EmmanuelOmale <eomale@gmail.com>
2023-11-11 13:22:50 +0100

    Calculator history model identification and DbContext implementation

---
+++ CalculatorApp/Entities/CalculatorHistory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalculatorApp.Entities
+{
+    public class CalculatorHistory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Expression { get; set; } = string.Empty;
+        public double Result { get; set; } 
+    }
+}
---
+++ CalculatorApp/Infrastructure/Data/CalculatorContext.cs
@@ -0,0 +1,13 @@
+﻿using CalculatorApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalculatorApp.Infrastructure.Data
+{
+    public class CalculatorContext : DbContext
+    {
+        public CalculatorContext(DbContextOptions<CalculatorContext> options) : base(options) { }
+
+        public DbSet<CalculatorHistory> CalculationHistories { get; set; }
+
+    }
+}
