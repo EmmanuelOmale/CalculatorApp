4304b81462e1f2922f27c68c7d59f2fd6e11738d
EmmanuelOmale <eomale@gmail.com>
2023-11-11 12:51:26 +0100

    The calculator controller implemented

---
+++ CalculatorApp/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ICalculatorService _calculatorService;
+        public CalculatorController(ICalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+        [HttpGet]
+        public IActionResult Calculate(double num1, double num2, string operator)
+        {
+           double result = _calculatorService.Calculate(num1, num2);
+            return Ok(result);
+        }
+    }
+}
